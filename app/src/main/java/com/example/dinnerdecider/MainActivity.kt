0c170238842d280001891cc7c369a5c0fa0e8b6e
Blessing Wangoi <wmugo@kabarak.ac.ke>
2025-01-17 10:27:14 +0300

    Activated decideBtn

--- app/src/main/java/com/example/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/example/dinnerdecider/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.dinnerdecider
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,16 +13,24 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.dinnerdecider.ui.theme.DinnerDeciderTheme
+import java.util.Random
 
 class MainActivity : ComponentActivity() {
+
+    val foodList= arrayListOf("Chinese","Hamburger","Pizza","McDonalds","Barros Pizza")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Find the TextView by its ID
-        val selectedFoodTxt = findViewById<TextView>(R.id.selectedFoodTxt)
+        val decideBtn=findViewById<Button>(R.id.decideBtn)
+        decideBtn.setOnClickListener{
+            val random = Random()
+            val randomFood=random.nextInt(foodList.count())
 
-        // Set the text
-        selectedFoodTxt.text = "Burger King"
+            // Find the TextView by its ID
+            val selectedFoodTxt = findViewById<TextView>(R.id.selectedFoodTxt)
+            selectedFoodTxt.text=foodList[randomFood]
+        }
     }
 }
